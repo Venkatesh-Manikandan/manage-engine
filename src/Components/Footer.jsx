70e959ec4a32c82318cedaa46328a6bc1f85b9ed
Venkatesh-Manikandan <venkateshmanikandan1998@gmail.com>
2024-09-05 20:43:29 +0530

    Footer section updated as a component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,178 +1,137 @@
 import '../Assets/Styles/Footer.css'
 import PromoBanner1 from '../Assets/Images/Footer/promo-banner1.jpg'
 import PromoBanner2 from '../Assets/Images/Footer/promo-banner2.jpg'
-import LinkedIn from '../Assets/Images/Footer/Linkedin.svg'
-import Twitter from '../Assets/Images/Footer/Twitter.svg'
-import Facebook from '../Assets/Images/Footer/Facebook.svg'
-import Instagram from '../Assets/Images/Footer/Instagram.png'
-import Youtube from '../Assets/Images/Footer/Youtube.svg'
 import FooterMenu from './FooterMenu'
 
 function Footer() {
 
+    const bannerDetails = [
+        {
+            imagePath: PromoBanner1,
+            link: 'https://www.manageengine.com/financial-services.html?pos=MEhome&loc=mefooter',
+            title: 'Transform your financial services and banking operations'
+        },
+        {
+            imagePath: PromoBanner2,
+            link: 'https://www.manageengine.com/iso20000/?pos=MEhome&loc=mefooter',
+            title: 'Comply with the ISO/IEC 20000 standard'
+        }
+    ]
+
     const footerMenuLinks = [
         {
             heading: 'Resources',
-            footerLink: 'Blog',
-            footerLink: 'Insights',
-            footerLink: 'Academy',
-            footerLink: 'Customer stories',
-            footerLink: 'Community',
-            footerLink: 'White papers',
-            footerLink: 'Product documentation',
+            link: [
+                    "Blog",
+                    "Insights",
+                    "Academy",
+                    "Customer stories",
+                    "Community",
+                    "White papers",
+                    "Product documentation",
+            ],
+            socialLink: [],
         },
         {
             heading: 'Company',
-            footerLink: 'About us',
-            footerLink: 'Customers',
-            footerLink: 'Analyst reports',
-            footerLink: 'Events',
-            footerLink: 'Newsroom',
-            footerLink: 'Partner with us',
-            footerLink: 'Careers',
-            footerLink: 'Sitemap',
+            link: [
+                "About us",
+                "Customers",
+                "Analyst reports",
+                "Events",
+                "Newsroom",
+                "Partner with us",
+                "Careers",
+                "Sitemap",
+            ],
+            socialLink: [],
         },
         {
             heading: 'Training and support',
-            footerLink: 'Contact support',
-            footerLink: 'Store',
-            footerLink: 'Marketplace',
-            footerLink: 'Training and certification',
+            link: [
+                "Contact support",
+                "Store",
+                "Marketplace",
+                "Training and certification",
+            ],
+            socialLink: [
+                {
+                    mediaLink: "https://www.linkedin.com/company/manageengine",
+                    mediaLink: "https://x.com/manageengine",
+                    mediaLink: "https://www.facebook.com/ManageEngine/",
+                    mediaLink: "https://www.instagram.com/manageengine/",
+                    mediaLink: "https://www.youtube.com/user/manageengine",
+                },
+            ]
         },
-        // {
-        //     heading: 'Connect with us:',
-        //     socialLink: '',
-        //     socialLink: '',
-        //     socialLink: '',
-        //     socialLink: '',
-        //     socialLink: '',
-        // }
+    ]
+
+    const footerBottomDetails = [
+        {
+            linkText: "About us",
+            targetLink: "", 
+        },
+        {
+            linkText: "EULA",
+            targetLink: "", 
+        },
+        {
+            linkText: "Terms of Service",
+            targetLink: "", 
+        },
+        {
+            linkText: "Security",
+            targetLink: "", 
+        },
+        {
+            linkText: "Compliance",
+            targetLink: "", 
+        },
+        {
+            linkText: "Privacy policy",
+            targetLink: "", 
+        },
+        {
+            linkText: "Cookie policy",
+            targetLink: "", 
+        },
+        {
+            linkText: "Affiliate program",
+            targetLink: "", 
+        },
+        {
+            linkText: "Newsletter",
+            targetLink: "", 
+        },
+        {
+            linkText: "Contact sales",
+            targetLink: "", 
+        },
+        {
+            linkText: "Our offices",
+            targetLink: "", 
+        },
+        {
+            linkText: "Global",
+            targetLink: "", 
+        },
     ]
 
     return(
         <footer>
             <div className="default_container">
+                
                 <div className="main_footer flex_row">
 
                     <div className="footer_column promo_banner_section">
-                        <a href="javascript:;" title="Transform your financial services and banking operations">
-                            <img src={PromoBanner1} alt="Transform your financial services and banking operations" />
-                        </a>
-                        <a href="javascript:;" title="Comply with the ISO/IEC 20000 standard">
-                            <img src={PromoBanner2} alt="Comply with the ISO/IEC 20000 standard" />
-                        </a>
-                    </div>
-                    
-                    <div className="footer_column">
-                        <h2>Resources</h2>
-                        <ul>
-                            <li>
-                                <a href="javascript:;" title="Blog">Blog</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Insights">Insights</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Academy">Academy</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Customer stories">Customer stories</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Community">Community</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="White papers">White papers</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Product documentation">Product documentation</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer_column">
-                        <h2>Company</h2>
-                        <ul>
-                            <li>
-                                <a href="javascript:;" title="About us">About us</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Customers">Customers</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Analyst reports">Analyst reports</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Events">Events</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Newsroom">Newsroom</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Partner with us">Partner with us</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Careers">Careers</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Sitemap">Sitemap</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer_column">
-                        <h2>Training and support</h2>
-                        <ul>
-                            <li>
-                                <a href="javascript:;" title="Contact support">Contact support</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Store">Store</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Marketplace">Marketplace</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" title="Training and certification">Training and certification</a>
-                            </li>
-                        </ul>
+                        
+                        {bannerDetails.map( bannerDetail => <a href={bannerDetail.link} title={bannerDetail.title} target='_blank'><img src={bannerDetail.imagePath} alt={bannerDetail.title} /></a>)}
 
-                        <div className='social_section'>
-                            <h2>Connect with us:</h2>
-                            <ul className='flex_row'>
-                                <li>
-                                    <a href="javascript:;" title="Follow us on LinkedIn"></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:;" title="Follow us on Twitter"></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:;" title="Follow us on Facebook"></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:;" title="Follow us on Instagram"></a>
-                                </li>
-                                <li>
-                                    <a href="javascript:;" title="Follow us on YouTube"></a>
-                                </li>
-                            </ul>
-                            <p><b>ManageEngine</b>  is a division of <b>Zoho Corp.</b></p>
-                        </div>
-                    </div>
-
-                </div>
-                {/* <div className="main_footer flex_row">
-
-                    <div className="footer_column promo_banner_section">
-                        <a href="javascript:;" title="Transform your financial services and banking operations">
-                            <img src={PromoBanner1} alt="Transform your financial services and banking operations" />
-                        </a>
-                        <a href="javascript:;" title="Comply with the ISO/IEC 20000 standard">
-                            <img src={PromoBanner2} alt="Comply with the ISO/IEC 20000 standard" />
-                        </a>
                     </div>
                     
                     {footerMenuLinks.map( footerMenuLinks => <FooterMenu menuLink={footerMenuLinks} />)}
 
-                </div> */}
+                </div>
             </div>
 
 
@@ -180,7 +139,7 @@ function Footer() {
                 <div className='default_container'>
                     <div className='footer_bottom_inner'>
                         <ul className='flex_row'>
-                            <li>
+                            {/* <li>
                                 <a href="javascript:;" title='About us'>About us</a>
                             </li>
                             <li>
@@ -212,10 +171,10 @@ function Footer() {
                             </li>
                             <li>
                                 <a href="javascript:;" title='Our offices'>Our offices</a>
-                            </li>
-                            {/* <li>
-                                <a href="javascript:;" title=''></a>
                             </li> */}
+
+                            {footerBottomDetails.map( footerBottomDetail => <li><a href='javascript:;' title={footerBottomDetail.linkText}>{footerBottomDetail.linkText}</a></li>)}
+                            
                         </ul>
 
                         <p>© 2024 <a href="https://www.zohocorp.com/?pos=MEhome&loc=mefooter" target='_blank' title='Zoho Corp'>Zoho Corporation Pvt. Ltd.</a> All rights reserved.</p>
--- src/Components/FooterMenu.jsx
+++ src/Components/FooterMenu.jsx
@@ -1,16 +1,57 @@
 function FooterMenu(props) {
 
     const {menuLink} =props
+    
+    const footerMenuLink = [];
+    for(const link of menuLink.link) {
+        footerMenuLink.push(<li><a href="javascript:;">{link}</a></li>)
+    }
 
-    console.log(menuLink);
+    // const footerSocialLink = [];
+    // for(const socialLink of menuLink.socialLink){
+    //     footerSocialLink.push(<li><a href={socialLink} title="Follow us on LinkedIn"></a></li>)
+    // }
     
-    
+    const socialLinks = [
+        {
+            mediaLink: "https://www.linkedin.com/company/manageengine",
+            title: "Follow us on LinkedIn",
+        },
+        {
+            mediaLink: "https://x.com/manageengine",
+            title: "Follow us on Twitter",
+        },
+        {
+            mediaLink: "https://www.facebook.com/ManageEngine/",
+            title: "Follow us on Facebook",
+        },
+        {
+            mediaLink: "https://www.instagram.com/manageengine/",
+            title: "Follow us on Instagram",
+        },
+        {
+            mediaLink: "https://www.youtube.com/user/manageengine",
+            title: "Follow us on YouTube",
+        },
+    ]
+
     return(
         <div className="footer_column">
             <h2>{menuLink.heading}</h2>
             <ul>
-                {/* {menuLink.link.map( (menuLinks) => (<li><a href="javascript:;" title={menuLinks.footerLink}>{menuLinks.footerLink}</a></li>))} */}
+                {footerMenuLink}
             </ul>
+            {menuLink.socialLink != '' ? 
+                <div className='social_section'>
+                    <h2>Connect with us:</h2>
+                    <ul className='flex_row'>
+                        {/* {footerSocialLink} */}
+                        {socialLinks.map( socialLink => <li><a href={socialLink.mediaLink} title={socialLink.title} target="_blank"></a></li>)}
+                    </ul>
+                    <p><b>ManageEngine</b>  is a division of <b>Zoho Corp.</b></p>
+                </div>
+            : ''}
+
         </div>
     )
 }
